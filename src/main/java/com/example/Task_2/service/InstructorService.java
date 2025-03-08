10e46e9886b1a1abf2813aee23957ee1329507fe
omar mohammed <omarb2515@gmail.com>
2025-03-08 22:18:00 +0200

    add instructor service

--- src/main/java/com/example/Task_2/service/InstructorService.java
+++ src/main/java/com/example/Task_2/service/InstructorService.java
@@ -1,2 +1,29 @@
-package com.example.Task_2.service;public class InstructorService {
+package com.example.Task_2.service;
+
+import com.example.Task_2.Table.Instructor;
+import com.example.Task_2.repository.InstructorRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class InstructorService {
+    private final InstructorRepository instructorRepository;
+
+    public InstructorService(InstructorRepository instructorRepository) {
+        this.instructorRepository = instructorRepository;
+    }
+
+    public List<Instructor> getAllInstructors() {
+        return instructorRepository.findAll();
+    }
+
+    public Optional<Instructor> getInstructorByID(Integer id) {
+        return instructorRepository.findById(id);
+    }
+
+    public Instructor getInstructorByEmail(String email) {
+        return instructorRepository.findInstructorByEmail(email);
+    }
 }
