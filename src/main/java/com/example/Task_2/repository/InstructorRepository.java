84ec3864ea8c898fc1f731b0156e0b1d54cd1b5e
omar mohammed <omarb2515@gmail.com>
2025-03-08 21:39:32 +0200

    add instructor repository

--- src/main/java/com/example/Task_2/repository/InstructorRepository.java
+++ src/main/java/com/example/Task_2/repository/InstructorRepository.java
@@ -1,2 +1,11 @@
-package com.example.Task_2.repository;public class InstructorRepository {
+package com.example.Task_2.repository;
+
+import com.example.Task_2.Table.Instructor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface InstructorRepository extends JpaRepository<Instructor, Integer> {
+    @Query("SELECT instructor FROM Instructor instructor where instructor.email=:email")
+    Instructor findInstructorByEmail(@Param("email") String email);
 }
