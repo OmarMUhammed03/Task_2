0377e64499dde8f593e99acfcf067f283b26aea2
omar mohammed <omarb2515@gmail.com>
2025-03-08 23:03:11 +0200

    update instructor repository

--- src/main/java/com/example/Task_2/repository/InstructorRepository.java
+++ src/main/java/com/example/Task_2/repository/InstructorRepository.java
@@ -1,6 +1,6 @@
 package com.example.Task_2.repository;
 
-import com.example.Task_2.Table.Instructor;
+import com.example.Task_2.model.Instructor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
