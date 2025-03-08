1b8117f7f87471dba8108e4c1ad827a0ad757a69
omar mohammed <omarb2515@gmail.com>
2025-03-08 22:17:45 +0200

    add course service

--- src/main/java/com/example/Task_2/service/CourseService.java
+++ src/main/java/com/example/Task_2/service/CourseService.java
@@ -1,2 +1,37 @@
-package com.example.Task_2.service;public class CourseService {
+package com.example.Task_2.service;
+
+import com.example.Task_2.Table.Course;
+import com.example.Task_2.repository.CourseRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CourseService {
+    private final CourseRepository courseRepository;
+
+    public CourseService(CourseRepository courseRepository) {
+        this.courseRepository = courseRepository;
+    }
+
+    public List<Course> getAllCourses() {
+        return courseRepository.findAll();
+    }
+
+    public Optional<Course> getCourseByID(Integer id) {
+        return courseRepository.findById(id);
+    }
+
+    public List<Course> getCoursesByInstructorId(Integer instructorId) {
+        return courseRepository.findCoursesByInstructorId(instructorId);
+    }
+
+    public void enrollStudent(Integer courseId, Integer studentId) {
+        courseRepository.enrollStudent(courseId, studentId);
+    }
+
+    public void unenrollStudent(Integer courseId, Integer studentId) {
+        courseRepository.unenrollStudent(courseId, studentId);
+    }
 }
