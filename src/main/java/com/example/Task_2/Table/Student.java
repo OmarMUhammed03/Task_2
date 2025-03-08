cf9ef1638abe99a439084cbd4af7847967f10936
omar mohammed <omarb2515@gmail.com>
2025-03-08 21:12:06 +0200

    add students table & its relations

---
+++ src/main/java/com/example/Task_2/Table/Student.java
@@ -0,0 +1,30 @@
+package com.example.Task_2.Table;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@Entity
+@Table(name = "Students")
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    @ManyToMany
+    @JoinTable(
+            name = "student_course", // Join table name
+            joinColumns = @JoinColumn(name = "student_id"),
+            inverseJoinColumns = @JoinColumn(name = "course_id")
+    )
+
+    private List<Course> courses;
+
+}
