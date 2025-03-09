3c092b18f04566170725068abfb8bd83cad8944d
omar mohammed <omarb2515@gmail.com>
2025-03-09 14:42:42 +0200

    update instructor model

--- src/main/java/com/example/Task_2/model/Instructor.java
+++ src/main/java/com/example/Task_2/model/Instructor.java
@@ -1,29 +1,67 @@
 package com.example.Task_2.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import lombok.Data;
 
+import java.util.ArrayList;
 import java.util.List;
 
-@Data
 @Entity
+@Table(name = "instructor")
 public class Instructor {
     @Id
     private Integer id;
 
     private String name;
 
+    public List<Course> getCourses() {
+        return courses;
+    }
+
+    public void setCourses(List<Course> courses) {
+        this.courses = courses;
+    }
+
     private String email;
 
     @OneToMany(mappedBy = "instructor")
-    private List<Course> courses;
+    @JsonIgnore
+    private List<Course> courses = new ArrayList<>();
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
 
     public Instructor(String name, String email) {
         this.name = name;
         this.email = email;
     }
+
 
     public Instructor() {
 
