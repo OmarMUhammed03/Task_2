daa32f7f39db656459a4fd6dfc27157c3b2f8b7c
omar mohammed <omarb2515@gmail.com>
2025-03-09 14:42:05 +0200

    update student model

--- src/main/java/com/example/Task_2/model/Student.java
+++ src/main/java/com/example/Task_2/model/Student.java
@@ -1,13 +1,14 @@
 package com.example.Task_2.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.Data;
 
+import java.util.ArrayList;
 import java.util.List;
 
-@Data
 @Entity
-@Table(name = "students")
+@Table(name = "student")
 public class Student {
 
     @Id
@@ -19,17 +20,50 @@ public class Student {
     private String email;
 
     @ManyToMany
+    @JsonIgnore
     @JoinTable(
             name = "student_course", // Join table name
             joinColumns = @JoinColumn(name = "student_id"),
             inverseJoinColumns = @JoinColumn(name = "course_id")
     )
 
-    private List<Course> courses;
+    private List<Course> courses = new ArrayList<>();
 
     public Student(String name, String email) {
         this.name = name;
         this.email = email;
+    }
+
+    public List<Course> getCourses() {
+        return courses;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setCourses(List<Course> courses) {
+        this.courses = courses;
+    }
+
+    public String getName() {
+        return name;
     }
 
     public Student() {
